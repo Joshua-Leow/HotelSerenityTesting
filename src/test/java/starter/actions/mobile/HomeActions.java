d790229305daf63e4413f72653153f33606d1288
Roland Ross L. Hadi <rolandrosshadi@yahoo.com>
2024-02-18 20:33:30 +0800

    Updated API, AS400 Mobile and Web Examples

---
+++ src/test/java/starter/actions/mobile/HomeActions.java
@@ -0,0 +1,11 @@
+package starter.actions.mobile;
+
+import net.serenitybdd.screenplay.Performable;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Click;
+import starter.pages.web.HomePage;
+
+public class HomeActions {
+
+
+}
---
+++ src/test/java/starter/actions/mobile/LoginActions.java
@@ -0,0 +1,14 @@
+package starter.actions.mobile;
+
+import net.serenitybdd.core.Serenity;
+import net.serenitybdd.screenplay.Performable;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.actions.Open;
+import starter.pages.web.LoginPage;
+
+public class LoginActions {
+
+
+}
