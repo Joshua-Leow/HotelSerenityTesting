d5794ae0c07be459e405fe8ee17cb943934b256a
joshua.leow <joshua.leow@gientech.com>
2024-02-20 16:09:39 +0800

    added authentication token getter & setter, and recall token.

--- src/test/java/starter/pages/API/LoginPage.java
+++ src/test/java/starter/pages/API/LoginPage.java
@@ -3,26 +3,29 @@ package starter.pages.API;
 public class LoginPage {
     private String username;
     private String password;
+    private String token;
+
 
     public LoginPage(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
-    public String getUsername() {
-        return this.username;
-    }
-
+    public String getUsername() { return this.username; }
     public String getPassword() {
         return this.password;
     }
+    public String getToken() { return this.token; }
 
     public void setUsername(String username) {
         this.username = username;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
+    public void setToken(String token) {
+        this.token = token;
+    }
+
 
 }
--- src/test/java/starter/steps/api/apiStepDefinitions.java
+++ src/test/java/starter/steps/api/apiStepDefinitions.java
@@ -6,6 +6,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.http.ContentType;
 import io.restassured.mapper.ObjectMapperType;
+import io.restassured.response.Response;
 import net.serenitybdd.core.Serenity;
 import net.serenitybdd.rest.SerenityRest;
 import net.serenitybdd.screenplay.Actor;
@@ -20,8 +21,9 @@ public class apiStepDefinitions {
 
     public String getCurrentEndpoint() {
         String environment = "environments." + Serenity.environmentVariables().getProperty("environment", "default");
-        return  Serenity.environmentVariables().getProperty(environment + ".restapi.baseurl");
+        return Serenity.environmentVariables().getProperty(environment + ".restapi.baseurl");
     }
+
     @Given("{actor} is logged in using username {string} and password {string}")
     public void userIsLoggedInUsingUsernameUsernameAndPasswordPassword(Actor actor, String username, String password) {
         LoginPage payload = new LoginPage(username, password);
@@ -32,11 +34,26 @@ public class apiStepDefinitions {
                 .accept(ContentType.JSON)
                 .contentType(ContentType.JSON).post().getBody().as(LoginPage.class, ObjectMapperType.GSON).getUsername();
         actor.remember("current_username", userName);
+
+        Response response = SerenityRest.given()
+                .baseUri(getCurrentEndpoint())
+                .basePath("/signin")
+                .body(payload, ObjectMapperType.GSON)
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .post();
+
+        String token = response.getBody().jsonPath().getString("token");
+        actor.remember("current_token", token);
     }
 
     @Then("{actor} should see the response authenticated successfully")
     public void userShouldSeeTheResponseAuthenticatedSuccessfully(Actor actor) {
         actor.recall("current_username");
+
+        String token = actor.recall("current_token");
+        System.out.println("Token: " + token);
+
         SerenityRest.lastResponse().then().statusCode(200);
     }
 }
