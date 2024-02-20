54530d4785be10f7b245fcf67bc5075a97e020a2
joshua.leow <joshua.leow@gientech.com>
2024-02-20 12:02:25 +0800

    updated serenity.conf chrome driver added "Successful User Registration" test case for Mobile. (Not tested)

--- src/test/java/starter/steps/mobile/mobileStepDefinitions.java
+++ src/test/java/starter/steps/mobile/mobileStepDefinitions.java
@@ -6,8 +6,11 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import net.serenitybdd.core.Serenity;
 import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.ensure.Ensure;
+import net.serenitybdd.screenplay.questions.page.TheWebPage;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import starter.actions.web.LoginActions;
+import starter.actions.web.RegisterActions;
 import starter.questions.HomePageQuestions;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -15,6 +18,9 @@ import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 public class mobileStepDefinitions {
     @Given("{actor} is on the mobile login page")
     public void userIsOnTheMobileLoginPage(Actor actor) {
+        actor.attemptsTo(
+                LoginActions.navigateToLoginPage()
+        );
     }
     @When("{actor} input username {string} and password {string} on the mobile")
     public void userInputUsernameUsernameAndPasswordPasswordOnTheMobile(Actor actor, String username, String password) {
@@ -33,6 +39,69 @@ public class mobileStepDefinitions {
     public void userShouldBeAuthenticatedSuccessfullyOnTheMobile(Actor actor) {
         actor.should(
                 seeThat(HomePageQuestions.isPrimaryAccountViewDetailsButtonVisible())
+        );
+    }
+
+    @When("{actor} clicks Sign Up button on the mobile")
+    public void userClicksSignUpButtonOnTheMobile(Actor actor) {
+        actor.attemptsTo(
+                LoginActions.clickSignUp()
+        );
+    }
+
+    @And("{actor} enters first name {string} on the mobile")
+    public void userEntersFirstNameOnTheMobile(Actor actor, String firstname) {
+        actor.attemptsTo(
+                RegisterActions.enterFirstName(firstname)
+        );
+    }
+
+    @And("{actor} enters last name {string} on the mobile")
+    public void userEntersLastNameOnTheMobile(Actor actor, String lastname) {
+        actor.attemptsTo(
+                RegisterActions.enterLastName(lastname)
+        );
+    }
+
+    @And("{actor} enters phone {string} on the mobile")
+    public void userEntersPhoneOnTheMobile(Actor actor, String phone) {
+        actor.attemptsTo(
+                RegisterActions.enterPhone(phone)
+        );
+    }
+
+    @And("{actor} enters email {string} on the mobile")
+    public void userEntersEmailOnTheMobile(Actor actor, String email) {
+        actor.attemptsTo(
+                RegisterActions.enterEmail(email)
+        );
+    }
+
+    @And("{actor} enters username {string} on the mobile")
+    public void userEntersUsernameOnTheMobile(Actor actor, String username) {
+        actor.attemptsTo(
+                RegisterActions.enterUsername(username)
+        );
+    }
+
+    @And("{actor} enters password {string} on the mobile")
+    public void userEntersPasswordOnTheMobile(Actor actor, String password) {
+        actor.attemptsTo(
+                RegisterActions.enterPassword(password)
+        );
+    }
+
+    @And("{actor} clicks Sign Up Register button on the mobile")
+    public void userClicksSignUpRegisterButtonOnTheMobile(Actor actor) {
+        actor.attemptsTo(
+                RegisterActions.clickSignUp()
+        );
+    }
+
+    @Then("{actor} redirects to login page on the mobile")
+    public void userRedirectsToLoginPageOnTheMobile(Actor actor) {
+        actor.attemptsTo(
+                Ensure.that(TheWebPage.currentUrl()).containsIgnoringCase("index")
         );
     }
 }
--- src/test/resources/features/mobile/UserAuthentication.feature
+++ src/test/resources/features/mobile/UserAuthentication.feature
@@ -5,6 +5,21 @@ Feature: Mobile User Authentication
 	Background:
 		Given User is on the mobile login page
 
+	Scenario Outline: Successful User Registration
+		When User clicks Sign Up button on the mobile
+		And User enters first name "<firstname>" on the mobile
+		And User enters last name "<lastname>" on the mobile
+		And User enters phone "<phone>" on the mobile
+		And User enters email "<email>" on the mobile
+		And User enters username "<username>" on the mobile
+		And User enters password "<password>" on the mobile
+		And User clicks Sign Up Register button on the mobile
+		Then User redirects to login page on the mobile
+		Examples:
+			| firstname | lastname | phone | email  | username | password |
+			| user      | user     | 12345 | ab@cde | user01   | user01   |
+		# email and username cannot exist
+
 	Scenario Outline: Successful User Authentication
 		When User input username '<Username>' and password '<Password>' on the mobile
 		And User submit the login form on the mobile
