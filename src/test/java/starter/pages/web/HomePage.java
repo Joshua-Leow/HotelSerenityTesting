91e4447b7d7044fd0be9d09fa00fa29185285c1d
jjjjleoww <josh.leow.2010@gmail.com>
2024-04-23 20:21:04 +0800

    correct home page locator

--- src/test/java/starter/pages/web/HomePage.java
+++ src/test/java/starter/pages/web/HomePage.java
@@ -4,9 +4,9 @@ import net.serenitybdd.screenplay.targets.Target;
 import net.thucydides.core.pages.PageObject;
 
 public class HomePage extends PageObject {
-    public static Target ABOUT_SIDE_BUTTON = Target.the("Profile Dropdown Button").locatedBy("//a[normalize-space()='Me']");
-    public static Target LOGIN_SIDE_BUTTON = Target.the(" Cloud Bank Logo").locatedBy("//img[@alt='banner']");
-    public static Target REGISTER_SIDE_BUTTON = Target.the("Go To Withdrawal Button").locatedBy("//a[contains(@href,'/account/withdraw')]//div[contains(@class,'panel-footer')]//div[contains(@class,'clearfix')]");
-    public static Target ROOMS_BUTTON = Target.the("Go To Withdrawal Button").locatedBy("//a[contains(@href,'/account/withdraw')]//div[contains(@class,'panel-footer')]//div[contains(@class,'clearfix')]");
-    public static Target AMENITIES_BUTTON = Target.the("Go To Withdrawal Button").locatedBy("//a[contains(@href,'/account/withdraw')]//div[contains(@class,'panel-footer')]//div[contains(@class,'clearfix')]");
+    public static Target ABOUT_SIDE_BUTTON = Target.the("About Side Button").locatedBy("#about");
+    public static Target LOGIN_SIDE_BUTTON = Target.the("Login Side Button").locatedBy("#login");
+    public static Target REGISTER_SIDE_BUTTON = Target.the("Register Side Button").locatedBy("#register");
+    public static Target ROOMS_BUTTON = Target.the("Rooms Button").locatedBy("#rooms");
+    public static Target AMENITIES_BUTTON = Target.the("Amenities Button").locatedBy("#amenities");
 }
